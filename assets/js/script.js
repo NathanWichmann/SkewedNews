15db98cb11d3f49987518fbc5c41077671bd8052
Kemal Demirgil <kemal.demirgil@hotmail.com>
2021-03-29 22:42:39 -0400

    commenting out nyt api script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,35 @@
-var submitButton = document.getElementById("submit-button");
-var outputs = document.getElementById("outputs");
-var topHeadline;
+// var submitButton = document.getElementById("submit-button");
+// var outputs = document.getElementById("outputs");
+// var topHeadline;
 
+// // function getTopHeadlineNews() {
+// //     var topic = $("#selectedOption :selected").text();
+// //     // console.log(topic);
+// //     var apiURL = 'https://api.nytimes.com/svc/topstories/v2/' + topic + '.json?api-key=VYG56iuOROV7djK9Bd23ePHnTOs1ea1S';
+// //     // console.log(apiURL);
+// //     fetch(apiURL).then(function (response) {
+// //         if (response.ok){
+// //             response.json().then(function (data) {
+// //                 localStorage.setItem('topHeadline', JSON.stringify(data));
+// //                 topHeadline = (JSON.parse(localStorage.getItem("topHeadline")));
+// //                 console.log(topHeadline);
+// //                 console.log(topHeadline.results);
+// //                 // console.log(data);
+// //                 for (var i = 0; i < 10; i++) {
+// //                     var card = $("<div>").addClass("card");
+// //                     var cardBody = $("<div>").addClass("card-body");
+// //                     var image = $("<img>").attr("src", topHeadline.results[i].multimedia[2].url);
+// //                     var cardTitle = $("<h4>").addClass("card-title").text(topHeadline.results[i].title);
+// //                     var cardText = $("<p>").addClass("card-text").text(topHeadline.results[i].abstract);
+// //                     var cardDate = $("<p>").addClass("card-text").text(topHeadline.results[i].created_date.substring(0,10));
+// //                     $(outputs).append(card.append(cardBody.append(image, cardTitle, cardDate, cardText)));
+// //                 }
+// //                 return;
+// //             });
+// //         } else {
+// //             alert("Error");
+// //         }
+// //     });
+// // };
 
-// REMOVE BOOTSTRAP COMPONENTS
-// REPLACE WITH MATERIALIZE
-
-// Top-stories documentation:
-// https://developer.nytimes.com/docs/top-stories-product/1/overview
-
-
-function getTopHeadlineNews() {
-    var topic = $("#selectedOption :selected").text();
-    // console.log(topic);
-    var apiURL = 'https://api.nytimes.com/svc/topstories/v2/' + topic + '.json?api-key=VYG56iuOROV7djK9Bd23ePHnTOs1ea1S';
-    // console.log(apiURL);
-    fetch(apiURL).then(function (response) {
-        if (response.ok){
-            response.json().then(function (data) {
-                localStorage.setItem('topHeadline', JSON.stringify(data));
-                topHeadline = (JSON.parse(localStorage.getItem("topHeadline")));
-                console.log(topHeadline);
-                console.log(topHeadline.results);
-                // console.log(data);
-                for (var i = 0; i < 10; i++) {
-                    var card = $("<div>").addClass("card");
-                    var cardBody = $("<div>").addClass("card-body");
-                    var image = $("<img>").attr("src", topHeadline.results[i].multimedia[2].url);
-                    var cardTitle = $("<h4>").addClass("card-title").text(topHeadline.results[i].title);
-                    var cardText = $("<p>").addClass("card-text").text(topHeadline.results[i].abstract);
-                    var cardDate = $("<p>").addClass("card-text").text(topHeadline.results[i].created_date.substring(0,10));
-                    $(outputs).append(card.append(cardBody.append(image, cardTitle, cardDate, cardText)));
-                }
-                return;
-            });
-        } else {
-            alert("Error");
-        }
-    });
-};
-
-
-submitButton.addEventListener("click", getTopHeadlineNews);
+// submitButton.addEventListener("click", getTopHeadlineNews);
