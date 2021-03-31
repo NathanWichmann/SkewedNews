1470499249cbd42b9f4a369e795e0ee01f5bb77e
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-03-30 21:42:11 -0400

    adding content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,39 @@
-// var submitButton = document.getElementById("submit-button");
-// var outputs = document.getElementById("outputs");
-// var topHeadline;
+var mostPopular = document.getElementById("most-popular");
+var mostPopularContent = document.getElementById("most-popular-content");
 
-// // function getTopHeadlineNews() {
-// //     var topic = $("#selectedOption :selected").text();
-// //     // console.log(topic);
-// //     var apiURL = 'https://api.nytimes.com/svc/topstories/v2/' + topic + '.json?api-key=VYG56iuOROV7djK9Bd23ePHnTOs1ea1S';
-// //     // console.log(apiURL);
-// //     fetch(apiURL).then(function (response) {
-// //         if (response.ok){
-// //             response.json().then(function (data) {
-// //                 localStorage.setItem('topHeadline', JSON.stringify(data));
-// //                 topHeadline = (JSON.parse(localStorage.getItem("topHeadline")));
-// //                 console.log(topHeadline);
-// //                 console.log(topHeadline.results);
-// //                 // console.log(data);
-// //                 for (var i = 0; i < 10; i++) {
-// //                     var card = $("<div>").addClass("card");
-// //                     var cardBody = $("<div>").addClass("card-body");
-// //                     var image = $("<img>").attr("src", topHeadline.results[i].multimedia[2].url);
-// //                     var cardTitle = $("<h4>").addClass("card-title").text(topHeadline.results[i].title);
-// //                     var cardText = $("<p>").addClass("card-text").text(topHeadline.results[i].abstract);
-// //                     var cardDate = $("<p>").addClass("card-text").text(topHeadline.results[i].created_date.substring(0,10));
-// //                     $(outputs).append(card.append(cardBody.append(image, cardTitle, cardDate, cardText)));
-// //                 }
-// //                 return;
-// //             });
-// //         } else {
-// //             alert("Error");
-// //         }
-// //     });
-// // };
+console.log(mostPopular.innerHTML);
+console.log(mostPopularContent.innerHTML);
+
+var newsContent;
+
+function getTopHeadlineNews() {
+    // var apiURL = 'http://api.mediastack.com/v1/news?&sources=en&access_key=8fcb8dd3df70c7830e622a83fed5dd6f';
+
+
+    var apiURL = 'https://api.mediastack.com/v1?sources&sources=en&access_key=8fcb8dd3df70c7830e622a83fed5dd6f&search=bbc';
+    fetch(apiURL).then(function (response) {
+        if (response.ok){
+            response.json().then(function (data) {
+                localStorage.setItem('news', JSON.stringify(data));
+                newsContent = (JSON.parse(localStorage.getItem("news")));
+                console.log(data);
+                return;
+            });
+        } else {
+            alert("Error");
+        }
+    });
+};
+getTopHeadlineNews();
+
 
 // submitButton.addEventListener("click", getTopHeadlineNews);
+
+// for (var i = 0; i < 10; i++) {
+//     var card = $("<div>").addClass("card");
+//     var cardBody = $("<div>").addClass("card-body");
+//     var image = $("<img>").attr("src", topHeadline.results[i].multimedia[2].url);
+//     var cardTitle = $("<h4>").addClass("card-title").text(topHeadline.results[i].title);
+//     var cardText = $("<p>").addClass("card-text").text(topHeadline.results[i].abstract);
+//     var cardDate = $("<p>").addClass("card-text").text(topHeadline.results[i].created_date.substring(0,10));
+//     $(outputs).append(card.append(cardBody.append(image, cardTitle, cardDate, cardText)));
