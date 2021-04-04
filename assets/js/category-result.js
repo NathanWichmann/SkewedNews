853d4db3912a785424c0c218acdd27245c726afc
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-04-03 23:51:36 -0400

    merging conflicts

--- assets/js/category-result.js
+++ assets/js/category-result.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var displayContent = document.querySelector("#display-content");
 
 var categoryName = localStorage.getItem("categoryName");
@@ -25,32 +24,4 @@ function run() {
     }
 }
 
-=======
-var displayContent = document.querySelector("#display-content");
-
-var categoryName = localStorage.getItem("categoryName");
-var capCategoryName = categoryName[0].toUpperCase() + categoryName.slice(1);
-console.log(capCategoryName);
-
-var fetchedResult = JSON.parse(localStorage.getItem("passedData"));
-console.log(fetchedResult);
-function run() {
-    $(searchedTitle).text(capCategoryName);
-    for (var i = 0; i < 20; i++) {
-        var card = $("<div>").css({
-            "margin-top" : "60px", 
-            "border-bottom": "5px solid grey", 
-            "border-top": "5px solid grey", 
-            "background-color" : "#212121",
-            "text-align" : "center"
-        });
-        var title = $("<span>").addClass("white-text").text(fetchedResult.articles[i].title).css({"font-size" : "24px"});
-        var cardContainer = $("<div>").css({"text-align" : "center"});
-        var cardImg = $("<img>").attr("src", fetchedResult.articles[i].urlToImage).height(300).width(400);
-        var link = $("<a>").attr("href", fetchedResult.articles[i].url).text("Click for more").css({"font-size" : "18px"});
-        $("#display-content").append(card.append(title).append(cardContainer.append(cardImg), link));
-    }
-}
-
->>>>>>> 2986ba2144fc243f6b5ee5b0d5a2ea90378992f7
 run();
--- assets/js/search-result2.js
+++ assets/js/search-result2.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var displayContent = document.querySelector("#display-content");
 var searchedTitle = document.getElementById("searchedTitle");
 
@@ -36,43 +35,4 @@ function run() {
     }
 }
 
-=======
-var displayContent = document.querySelector("#display-content");
-var searchedTitle = document.getElementById("searchedTitle");
-
-var selectedNewsSource = localStorage.getItem("selectedSource");
-
-if (selectedNewsSource === "bbc-news") {
-    selectedNewsSource = "BBC News";
-}
-if (selectedNewsSource === "fox-news") {
-    selectedNewsSource = "Fox News";
-}
-if (selectedNewsSource === "cnn") {
-    selectedNewsSource = "CNN";
-}
-
-
-var fetchedResult = JSON.parse(localStorage.getItem("searchedData2"));
-console.log(fetchedResult);
-
-function run() {
-    $(searchedTitle).text(selectedNewsSource);
-    for (var i = 0; i < 20; i++) {
-        var card = $("<div>").css({
-            "margin-top" : "60px", 
-            "border-bottom": "5px solid grey", 
-            "border-top": "5px solid grey", 
-            "background-color" : "#212121",
-            "text-align" : "center"
-        });
-        var title = $("<span>").addClass("white-text").text(fetchedResult.articles[i].title).css({"font-size" : "24px"});
-        var cardContainer = $("<div>").css({"text-align" : "center"});
-        var cardImg = $("<img>").attr("src", fetchedResult.articles[i].urlToImage).height(300).width(400);
-        var link = $("<a>").attr("href", fetchedResult.articles[i].url).text("Click for more").css({"font-size" : "18px"});
-        $("#display-content").append(card.append(title).append(cardContainer.append(cardImg), link));
-    }
-}
-
->>>>>>> 2986ba2144fc243f6b5ee5b0d5a2ea90378992f7
 run();
